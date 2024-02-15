4f93698432735c5576ef9fd1b8102fdeca71bb61
v-lane <victoria.lane.m@gmail.com>
2024-02-15 18:07:04 -0500

    refactored with index.js as main file

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,28 +2,21 @@ const request = require('request');
 
 const URL = "https://api.thecatapi.com/v1/breeds/search?q=";
 
-const getSearchUrl = (anyURL, searchCriteria) => {
-  return anyURL + searchCriteria;
-};
-
-const userInput = process.argv[2];
-
-const searchUrl = getSearchUrl(URL, userInput);
-
-request.get(searchUrl, (error, response, body) => {
-
-  if (error) {
-    console.log("*** REQUEST FAILED *** \nURL does not exist. \nError details:", error);
-  } else {
-    const data = JSON.parse(body);
-    if (data === " ") {
-      console.log(`nothing data`);
+const fetchBreedDescription = function(breedName, callback) {
+  const searchUrl = URL + breedName;
+  request.get(searchUrl, (error, _response, body) => {
+    if (error) {
+      callback(error, null);
     }
     try {
+      const data = JSON.parse(body);
       const description = data[0].description;
-      console.log(description);
+      callback(null, description);
     } catch (error) {
-      console.log("This cat breed does not exist.");
+      callback(error, null);
     }
-  }
-});
+  });
+};
+
+
+module.exports = { fetchBreedDescription };
